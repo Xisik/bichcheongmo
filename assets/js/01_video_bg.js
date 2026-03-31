2067b0ca10f9e975e295b64c35a33a0dba95a445
kaede982006 <kaede982006@gmail.com>
2026-04-01 00:41:54 +0900

    41

--- assets/js/01_video_bg.js
+++ assets/js/01_video_bg.js
@@ -9,6 +9,8 @@
     '?autoplay=1&mute=1&loop=1&playlist=dQw4w9WgXcQ' +
     '&controls=0&disablekb=1&fs=0&modestbranding=1' +
     '&iv_load_policy=3&rel=0&playsinline=1';
+  iframe.setAttribute('width', '100%');
+  iframe.setAttribute('height', '100%');
   iframe.allow = 'autoplay; encrypted-media';
   iframe.tabIndex = -1;
   iframe.title = '';
