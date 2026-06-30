6ab81b520081adc6ddd42ab3b738c413bfd78679
GitHub Action <action@github.com>
2026-06-30 23:40:44 +0000

    chore: sync payments from Notion

--- assets/js/60_payments_data.js
+++ assets/js/60_payments_data.js
@@ -6,7 +6,7 @@
   ui.payments = ui.payments || {};
   ui.payments.data = {
   "_metadata": {
-    "lastUpdated": "2026-06-30T22:23:44.769Z",
+    "lastUpdated": "2026-06-30T23:40:44.526Z",
     "syncStatus": "success",
     "errorMessage": null,
     "paymentsCount": 1,
